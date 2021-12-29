45ded0df52d8a5e6cbd107c4fea530381671bb48
Jayant Kodwani <JAYANT.KODWANI@YAHOO.IN>
2021-12-29 16:50:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,38 +4,23 @@ import pickle
 
 
 app = Flask(__name__)
-#model = pickle.load(open('email_open_model.sav','rb'))
-
 
 @app.route('/')
 def home():
     #return 'Hello World'
     return render_template('home.html')
-    #return render_template('index.html')
 
 @app.route('/predict',methods = ['POST'])
 def predict():
-    return render_template('home.html')
-    #int_features = [float(x) for x in request.form.values()]
-    #final_features = [np.array(int_features)]
-    #prediction = model.predict(final_features)
-    #print(prediction[0])
-
-    #output = round(prediction[0], 2)
-    #return render_template('home.html', prediction_text="Estimated number of opens {}".format(prediction[0]))
+    return render_template('home.html')   
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     '''
     For direct API calls trought request
     '''
-    #data = request.get_json(force=True)
-    #prediction = model.predict([np.array(list(data.values()))])
-    #output = prediction[0]
-    output = 'Hello'
+        output = 'Hello'
     return jsonify(output)
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
